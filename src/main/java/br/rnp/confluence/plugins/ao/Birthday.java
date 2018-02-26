69c35de25418907199cbd29cc9745234092ef882
Jose Flexa <jose.flexa@gmail.com>
2018-02-25 22:56:57 -0300

    add model class/reference on atlassian-plugin/maven references

---
+++ src/main/java/br/rnp/confluence/plugins/ao/Birthday.java
@@ -0,0 +1,14 @@
+package br.rnp.confluence.plugins.ao;
+
+import net.java.ao.Entity;
+
+public interface Birthday extends Entity {
+    
+    String getFullName();
+
+    void setFullName(String fullName);
+
+    String getDate();
+
+    void setDate(String date);
+}
